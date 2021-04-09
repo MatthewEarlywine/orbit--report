54625c0abf5b1039177a2dad4b5d2312a4f70087
Matt <matthewearlywine@gmail.com>
2021-04-08 20:23:51 -0500

    Orbit-Report v006

--- src/app/satellite.ts
+++ src/app/satellite.ts
@@ -4,6 +4,7 @@ export class Satellite {
     type: string;
     operational: boolean;
     launchDate: string;
+  //searchParams: any;
 
     constructor(name: string, type: string, launchDate: string, orbitType: string, operational: boolean){
         this.name = name;
